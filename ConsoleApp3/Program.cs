7766369f257c38d0f72d77ca70e1dfeded59f7c3
Rozicleide <roziibr@hotmail.com>
2022-02-16 17:26:53 -0400

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Numeroifelse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            if (x > 0)
+            {
+                Console.WriteLine("Número positivo ");
+            }
+            else if (x < 0)
+            {
+                Console.WriteLine("Número negativo");
+            }
+            else
+            {
+                Console.WriteLine("´Número zero");
+            }
+                Console.WriteLine("Fim do Programa");
+                Console.ReadKey();
+        }
+            
+    }
+}
